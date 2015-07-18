7011cf7bdbd695e6450cb5713864852e78530ac1
Vitaly Lipatov <lav@etersoft.ru>
2015-07-19 02:54:34 +0300

    add test for error status and fix it in eterpack

---
+++ tests/eterpack-errstatus.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+BASEDIR=/tmp/eterbackup-TD
+ETERPACK=$(pwd)/../bin/eterpack
+
+rm -rf $BASEDIR
+
+. ./eterbackup-functions.sh
+
+create_tree $BASEDIR/sample
+
+cd $BASEDIR || exit 1
+
+$ETERPACK update sample packed && fatal "update with unexists dir!"
+
+#$ETERPACK update sample packed || fatal "can't pack sapmle"
+
+$ETERPACK extract packed1 unpacked && fatal "extract from unexists dir!"
+
+echo "Done! OK!"
+echo "Please, check and remove $BASEDIR"
+#rm -rf $BASEDIR
