8df91d916389cfc2a832534afc0d71c33616e4a4
Vitaly Lipatov <lav@etersoft.ru>
2017-06-08 14:59:26 +0300

    restore eterpack_changed-test (it is allowed to use any source name dir)

--- tests/eterpack-changed-test.sh
+++ tests/eterpack-changed-test.sh
@@ -28,13 +28,17 @@ $ETERPACK update sample1 packed1 || exit 1
 $ETERPACK update sample2 packed2 || exit 1
 
 # TODO: we use other dir for update (2 and next 3): we lost prev. metainfo here
+if false ; then
 # FIXME: it is not allowed update archive from different named dirs
 mv sample3 sample3-
 mv sample2 sample3
 $ETERPACK update sample3 packed3 || exit 1
 mv sample3 sample2
 mv sample3- sample3
+else
+$ETERPACK update sample2 packed3 || exit 1
 $ETERPACK update sample3 packed3 || exit 1
+fi
 
 # change and pack
 $ETERPACK update sample4 packed4 || exit 1
