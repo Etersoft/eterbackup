70e871f242e9773a488bb30767921ee3adfb3b3b
Vitaly Lipatov <lav@etersoft.ru>
2018-01-31 03:09:42 +0300

    add shellcheck script, do small fixes

--- check_code.sh
+++ check_code.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
-checkbashisms -f bin/* || exit
-echo "Done."
-#checkbashisms -f Makefile eterbackup.spec
+
+# http://mywiki.wooledge.org/Bashism
+# https://wiki.ubuntu.com/DashAsBinSh
+
+EXCL=-eSC2086,SC2039,SC2034,SC2068,SC2155
+
+# TODO:
+# SC2154: pkg_filenames is referenced but not assigned.
+# SC2002: Useless cat.
+EXCL="$EXCL,SC2154,SC2002"
+
+if [ -n "$1" ] ; then
+    shellcheck $EXCL "$1"
+    exit
+fi
+
+checkbashisms -f bin/*
+
+shellcheck $EXCL \
+	bin/*
